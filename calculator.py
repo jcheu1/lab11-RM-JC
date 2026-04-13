376d106118f38e3a9a90e77549c2ad4406e60c64
rmedi2112 <rmedina9383@gmail.com>
2026-04-13 16:53:07 -0400

    Implement basic calculator functions in calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    return math.log(b, a)
+    
+def exponent(a, b):
+    return a ** b
